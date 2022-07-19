5bfcaecea91ee97cfe2ac5893a1f55a1d8fe879b
Victor Prindyn <victorprindyn.ua@gmail.com>
2022-07-19 17:33:07 +0300

    added gender ico"

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,5 +1,7 @@
 import React, { useId } from "react"
 import "./contacts.css"
+import maleImg from "../../img/male.png"
+import femaleImg from "../../img/female.png"
 
 function CreateContactsList(props) {
   const id = useId()
@@ -12,6 +14,13 @@ function CreateContactsList(props) {
               <h4 id={id + "name"} className="name">
                 {contact.firstName} {contact.lastName}
               </h4>
+              {contact.gender === "male" ? (
+                <img alt="male" src={maleImg} className="gender-img"></img>
+              ) : contact.gender === "female" ? (
+                <img alt="female" src={femaleImg} className="gender-img"></img>
+              ) : (
+                <h5>Unknown</h5>
+              )}
               <h5 className="number">{contact.phone}</h5>
             </li>
           )
